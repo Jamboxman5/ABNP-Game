bdedf68e7463fab825b364dcdf42d8231f6358f2
Jamboxman5 <samthompson1818@gmail.com>
2025-08-08 14:31:07 -0400

    Refined move packets to work with entity IDs rather than usernames

--- core/src/me/jamboxman5/abnpgame/net/packets/Packet02Move.java
+++ core/src/me/jamboxman5/abnpgame/net/packets/Packet02Move.java
@@ -6,20 +6,20 @@ import me.jamboxman5.abnpgame.net.GameServer;
 public class Packet02Move extends Packet {
 
 	private double x, y, rotation;
-	private String username;
+	private String entityID;
 	
 	public Packet02Move(byte[] data) {
 		super(02);
 		String[] dataArray = readData(data).split(",");
-		this.username = dataArray[0];
+		this.entityID = dataArray[0];
 		this.x = Double.parseDouble(dataArray[1]);
 		this.y = Double.parseDouble(dataArray[2]);
 		this.rotation = Double.parseDouble(dataArray[3]);
 	}
 	
-	public Packet02Move(String username, double x, double y, double rotation) {
+	public Packet02Move(String entityID, double x, double y, double rotation) {
 		super(02);
-		this.username = username;
+		this.entityID = entityID;
 		this.x = x;
 		this.y = y;
 		this.rotation = rotation;
@@ -37,10 +37,10 @@ public class Packet02Move extends Packet {
 
 	@Override
 	public byte[] getData() {
-		return ("02" + this.username + "," + this.x + "," + this.y + "," + this.rotation).getBytes();
+		return ("02" + this.entityID + "," + this.x + "," + this.y + "," + this.rotation).getBytes();
 	}
 	
-	public String getUsername() { return username; }
+	public String getUsername() { return entityID; }
 	public double getX() { return x; }
 	public double getY() { return y; }
 	public float getRotation() { return (float) rotation; }
