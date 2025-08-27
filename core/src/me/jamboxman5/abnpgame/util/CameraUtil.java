de428059c9a4489941240c91b1ec15e71929f4cd
Jamboxman5 <samthompson1818@gmail.com>
2025-08-27 03:54:52 -0400

    Added camera utility class with function to convert screen coordinates to world coordinates

---
+++ core/src/me/jamboxman5/abnpgame/util/CameraUtil.java
@@ -0,0 +1,25 @@
+package me.jamboxman5.abnpgame.util;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.math.Vector2;
+
+public class CameraUtil {
+
+    public static Vector2 getWorldPosScreenCenterDisplacement(PerspectiveCamera camera, float screenX, float screenY) {
+
+        float worldPerPixel = (2f * camera.position.y * (float)Math.tan(Math.toRadians(camera.fieldOfView) / 2f))
+                / Gdx.graphics.getHeight();
+
+        float dxPixels = screenX - Gdx.graphics.getWidth() / 2f;
+        float dzPixels = screenY - Gdx.graphics.getHeight() / 2f;
+
+// Screen Y is inverted, so flip sign if needed
+        float dxWorld = dxPixels * worldPerPixel;
+        float dzWorld = dzPixels * worldPerPixel;
+
+        return new Vector2(dxWorld, dzWorld);
+
+    }
+
+}
